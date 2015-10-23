eb8375520b7b4cf3ce362800862b0a84258cf2fd
Maxwell Krause <maxwell.l.krause@gmail.com>
2015-10-23 13:19:59 -0400

    init commit

---
+++ main.js
@@ -0,0 +1,56 @@
+var photos = [
+  {
+    "albumName": "Bear",
+    "imgs": [
+      "bear/bear_01.jpg",
+      "bear/bear_02.jpg",
+      "bear/bear_03.jpg",
+      "bear/bear_04.jpg",
+      "bear/bear_05.jpg",
+      "bear/bear_06.jpg",
+    ]
+  }
+
+  },
+  {
+    "albumName": "Greece",
+    "imgs": [
+      "grece/greece_01.jpg"
+      "grece/greece_02.jpg"
+      "grece/greece_03.jpg"
+      "grece/greece_04.jpg"
+      "grece/greece_05.jpg"
+      "grece/greece_06.jpg"
+      "grece/greece_07.jpg"
+    ]
+
+  }
+
+  },
+  {
+    "albumName": "My Love",
+    "imgs": {
+
+    }
+  },
+  {
+    "albumName" : "Wedding",
+    "imgs": {
+
+    }
+
+  },
+  {
+    "albumName" : "Family",
+    "imgs": {
+
+    }
+  },
+  {
+    "albumName" : "Friends",
+    "imgs": {
+
+    }
+
+  }
+];
