416df13f5c4ba92e88a1e7e4461a34eaf15ff0c1
Maxwell Krause <maxwell.l.krause@gmail.com>
2015-10-24 19:09:12 -0400

    after satruday

--- main.js
+++ main.js
@@ -1,56 +1,118 @@
-var photos = [
+var photoData = [
   {
-    "albumName": "Bear",
-    "imgs": [
-      "bear/bear_01.jpg",
-      "bear/bear_02.jpg",
-      "bear/bear_03.jpg",
-      "bear/bear_04.jpg",
-      "bear/bear_05.jpg",
-      "bear/bear_06.jpg",
+    albumName: "Bear",
+    imgs: [
+      "img/bear/bear_01.jpg",
+      "img/bear/bear_02.jpg",
+      "img/bear/bear_03.jpg",
+      "img/bear/bear_04.jpg",
+      "img/bear/bear_05.jpg",
+      "img/bear/bear_06.jpg",
     ]
-  }
-
   },
+
   {
-    "albumName": "Greece",
-    "imgs": [
-      "grece/greece_01.jpg"
-      "grece/greece_02.jpg"
-      "grece/greece_03.jpg"
-      "grece/greece_04.jpg"
-      "grece/greece_05.jpg"
-      "grece/greece_06.jpg"
-      "grece/greece_07.jpg"
+    albumName: "Greece",
+    imgs: [
+      "img/greece/greece_01.jpg",
+      "img/greece/greece_02.jpg",
+      "img/greece/greece_03.jpg",
+      "img/greece/greece_04.jpg",
+      "img/greece/greece_05.jpg",
+      "img/greece/greece_06.jpg",
+      "img/greece/greece_07.jpg",
     ]
+  },
 
-  }
-
+  {
+    albumName: "Love",
+    imgs: [
+      "img/love/love_01.jpg",
+      "img/love/love_02.jpg",
+      "img/love/love_03.jpg",
+      "img/love/love_04.jpg",
+      "img/love/love_05.jpg",
+      "img/love/love_06.jpg",
+      "img/love/love_07.jpg",
+    ]
   },
   {
-    "albumName": "My Love",
-    "imgs": {
-
-    }
+    albumName : "Wedding",
+    imgs: [
+      "img/wedding/wedding_01.jpg",
+      "img/wedding/wedding_02.jpg",
+      "img/wedding/wedding_03.jpg",
+      "img/wedding/wedding_04.jpg",
+    ]
   },
   {
-    "albumName" : "Wedding",
-    "imgs": {
-
-    }
-
+    albumName : "Family",
+    imgs: [
+      "img/family/family_01.jpg",
+      "img/family/family_02.jpg",
+      "img/family/family_03.jpg",
+      "img/family/family_04.jpg",
+      "img/family/family_05.jpg",
+      "img/family/family_06.jpg",
+    ]
   },
   {
-    "albumName" : "Family",
-    "imgs": {
-
-    }
-  },
-  {
-    "albumName" : "Friends",
-    "imgs": {
-
-    }
-
+    albumName : "Friends",
+    imgs: [
+      "img/friends/friends_01.jpg",
+      "img/friends/friends_02.jpg",
+      "img/friends/friends_03.jpg",
+      "img/friends/friends_04.jpg",
+      "img/friends/friends_05.jpg",
+      "img/friends/friends_06.jpg",
+    ]
   }
 ];
+
+
+
+//http://stackoverflow.com/questions/8648629/how-to-print-array-in-underscore-js-template
+//props to Drew M and Jacob O
+
+  var photoTemplate = _.template($('#albumTmpl').html());
+  var photoTemplate2 = _.template($('#galTmpl').html());
+
+  var photoHTMLalb = "";
+  _.each(photoData, function (el){
+    photoHTMLalb += photoTemplate(el);
+  });
+  var photoHTMLgal = "";
+  _.each(photoData, function (el){
+    photoHTMLgal += photoTemplate2(el);
+  });
+
+var selectedAlbum = _.map(photoData, function(el,idx,arr){
+    return {albumName : el.albumName};
+});
+
+$(document).ready(function () {
+  $(".album-view").html(photoHTMLalb);
+  $('.gallery-view').html(photoHTMLgal);
+
+//   var albSelector =  .each(photos, function(name){
+//     if (albSelector === el.photos.albumName) {
+//       $(albSelector).addClass ("active-selection");
+//     }
+//   });
+
+          // selectedAlbum += el.albumName;
+          // $(".album-view").removeClass('active-section');
+          // $(".gallery-view").addClass('active-section');
+          // $(selectedAlbum).removeClass('hidden-section');
+// });
+  //View Switches
+  // $(selectedAlbum > a).on(click, function(){
+  //   event.preventDefault();
+  //   $(".album-view").removeClass('active-section');
+  //   $(".gallery-view").addClass('active-section');
+  //   $(".Bear").addClass('active-section');
+  //
+  // });
+
+
+});
